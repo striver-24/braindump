a4c82852667097103f2adc9048395aac0093f897
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-11-23 03:44:43 +0530

    components defined

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 const News = () => {
   return (
-    <div>News</div>
+    <div className='news'>
+        <header className='news-header'>News Header</header>
+        <div className='news-content'>
+            <div className='navbar'>
+                <div className='user'>User</div>
+                <nav className='categories'>Categories</nav>
+            </div>
+            <div className='news-section'>
+                <div className='headline'>Headline</div>
+                <div className='news-grid'>News Grid</div>
+            </div>
+            <div className='my-blogs'>My Blogs</div>
+        </div>
+    </div>
   )
 }
 
