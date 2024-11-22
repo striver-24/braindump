1dd2ca53ef858325e115d97aebdb0ee4cb976906
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-11-23 03:33:43 +0530

    New Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import News from './components/news';
+
+const App = () => {
+  return (
+    
+    <div className='container'>
+      <div className='news-blog-app'>
+        <News />
+      </div>
+    </div>
+  )
+}
+
+export default App;
