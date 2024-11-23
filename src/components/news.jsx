5bf54bbdc4d96f72a13d0aeeaab567071d6458f9
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-11-23 22:30:02 +0530

    Finalising Layout for Landing Page

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -2,29 +2,53 @@ import React from 'react';
 import Weather from './weather';
 import Calender from './calender';
 import './News.css';
+import Logo from '../assets/Logo_BrainDump.svg'; // Import the SVG logo
 
 const News = () => {
   return (
-    <div className='news'>
-        <header className='news-header'>News Header</header>
-        <div className='news-content'>
-            <div className='navbar'>
-                <div className='user'>User</div>
-                <nav className='categories'>Categories</nav>
+    <div className="news">
+        <header className="news-header">
+            <img src={Logo} alt="BrainDump Logo" className='logo-image' /> {/* Add the logo here */}
+            <h1 className='logo'>BrainDump</h1>
+            <nav className="nav-links">
+                <a href="#home">Home</a>
+                <a href="#news">News</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
+            </nav>
+            <div className='search-bar'>
+                <form>
+                    <input type="text" placeholder='Search Here...' />
+                    <button type='submit'>
+                        <i className='fa-solid fa-magnifying-glass'></i>
+                    </button>
+                </form>
             </div>
-            <div className='news-section'>
-                <div className='headline'>Headline</div>
-                <div className='news-grid'>News Grid</div>
+        </header>
+        <div className="news-content">
+            <div className="navbar">
+                <div className="user">User  Profile</div>
+                <nav className="categories">Categories</nav>
             </div>
-            <div className='my-blogs'>My Blogs</div>
-            <div className='weather-calendar'>
+            <div className="news-section">
+                <div className="headline">Latest Headlines</div>
+                <div className="news-grid">
+                    <div className="news-article">
+                        <h2>Article Title</h2>
+                        <p>Summary of the article goes here...</p>
+                        <a href="#" className="read-more">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div className="my-blogs">My Blogs</div>
+            <div className="weather-calendar">
                 <Weather />
                 <Calender />
-            </div>  
+            </div>
         </div>
-        <footer className="news-footer">Footer</footer>
+        <footer className="news-footer">Footer Content</footer>
     </div>
-  )
+  );
 }
 
 export default News;
